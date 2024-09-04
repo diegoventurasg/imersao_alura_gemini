c53e8ea94c10b4ad19417f1f0b0255a1413418ec
Diego Ventura <diegoventurasg@gmail.com>
2024-09-04 09:22:26 -0300

    aula 03

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Sirius",
+        descricao: "Sirius, a estrela mais brilhante visível a olho nu da Terra, é uma verdadeira joia celeste. Localizada na constelação do Cão Maior, a apenas 8,6 anos-luz de distância, ela fascina observadores há milênios. Seu brilho intenso, cerca de 25 vezes maior que o do Sol, a torna um ponto de referência no céu noturno. Além disso, Sirius é um sistema estelar binário, composto por uma estrela branca brilhante e uma anã branca, o que a torna ainda mais interessante para os astrônomos.",
+        link: "https://pt.wikipedia.org/wiki/Sirius"
+    },
+    {
+        titulo: "Betelgeuse",
+        descricao: "Betelgeuse é uma supergigante vermelha na constelação de Orion. É uma das estrelas mais brilhantes do céu e varia em brilho ao longo do tempo. ",
+        link: "https://pt.wikipedia.org/wiki/Betelgeuse"
+    },
+    {
+        titulo: "Polaris",
+        descricao: "Polaris, também conhecida como Estrela do Norte, é a estrela mais brilhante da constelação da Ursa Menor. Ela é usada como ponto de referência para navegação há séculos.",
+        link: "https://pt.wikipedia.org/wiki/Polaris"
+    },
+];
